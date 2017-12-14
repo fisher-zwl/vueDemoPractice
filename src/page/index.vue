bdf2cbf35daa7d9afb82103bff0eb5b8cbb118ea
郑文龙 <1426034446@qq.com>
2017-12-14 10:38:58 +0800

    + 初始化vue pc端项目

---
+++ src/page/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>index page</div>
+</template>
+<script>
+export default {
+  created () {
+    this.$api.get('topics', null, r => {
+      console.log(r)
+    })
+  }
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/page/index'
+import Content from '@/page/content'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Index
+    }, {
+      path: '/content/:id',
+      component: Content
+    }
+  ]
+})
